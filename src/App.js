de4898340de8b2840f708678153e3112403cb7b5
unknown <y.perrot@outlook.fr>
2021-04-21 22:50:08 +0200

    Add calcul logic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 
 const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const equation = ["/", "x", "-", "+", "="]
+const equation = ["/", "x", "-", "+"]
 
 class App extends React.Component {
 
@@ -11,13 +11,81 @@ class App extends React.Component {
 		this.state = {
 			equationString: '',
 			inputString: '',
-			isOnResult: false
+			isOnResult: false, 
+			isOnEquation: false
 		}
 		this.insertInput = this.insertInput.bind(this)
 	}
 
 	insertInput(value) {
 
+		if (value === "RESET") {
+			this.setState({
+				equationString: '',
+				inputString: '',
+				isOnResult: false,
+				isOnEquation: false
+			})
+		}
+
+		if (value === "=") {
+			if (this.state.isOnEquation || this.state.isOnResult) return
+
+			try {
+				const res = eval(this.state.equationString.replace("x", "*"))
+				this.setState({
+					equationString: this.state.equationString + "=" + res,
+					inputString: res,
+					isOnResult: true
+				})
+			} catch (error) {
+				this.setState({
+					equationString: '',
+					inputString: "INVALID EQUATION",
+					isOnResult: true
+				})
+			}
+		}
+		
+		if (this.state.inputString.length >= 17) {
+			let tmpInput = this.state.inputString
+			this.setState({
+				inputString: 'DIGITAL LIMITE MET'
+			})
+			setTimeout((previousIn) => { this.setState({inputString: previousIn}) }, 3000, tmpInput)
+			return
+		}
+		
+		if (equation.includes(value)) {
+			this.setState({
+				equationString: this.state.equationString + value, 
+				inputString: '' + value, 
+				isOnEquation: true
+			})
+		}
+
+		if (number.includes(value) || value === '.') {
+
+
+			if (this.state.isOnResult) {
+				this.setState({
+					equationString: '' + value, 
+					inputString: '' + value,
+					isOnResult: false
+				})
+			} else if (this.state.isOnEquation) {
+				this.setState({
+					equationString: this.state.equationString + value, 
+					inputString: '' + value,
+					isOnEquation: false
+				})
+			} else {
+				this.setState({
+					equationString: this.state.equationString + value, 
+					inputString: this.state.inputString + value, 
+				})
+			}
+		}
 	}
 
 	render() {
@@ -26,23 +94,23 @@ class App extends React.Component {
 			<p className="equation">{this.state.equationString}</p>
 			<p className="input">{this.state.inputString}</p>
 			<div className="keyboard">
-				<button className="reset">AC</button>
-				<button className="divide">/</button>
-				<button className="multiply">x</button>
-				<button className="seven">7</button>
-				<button className="eight">8</button>
-				<button className="nine">9</button>
-				<button className="minus">-</button>
-				<button className="four">4</button>
-				<button className="five">5</button>
-				<button className="six">6</button>
-				<button className="plus">+</button>
-				<button className="one">1</button>
-				<button className="two">2</button>
-				<button className="three">3</button>
-				<button className="equal">=</button>
-				<button className="zero">0</button>
-				<button className="dot">.</button>
+				<button className="reset" onClick={() => this.insertInput("RESET")}> AC </button>
+				<button className="divide" onClick={() => this.insertInput("/")}> / </button>
+				<button className="multiply" onClick={() => this.insertInput("x")}> x </button>
+				<button className="seven" onClick={() => this.insertInput(7)}> 7 </button>
+				<button className="eight" onClick={() => this.insertInput(8)}> 8 </button>
+				<button className="nine" onClick={() => this.insertInput(9)}> 9 </button>
+				<button className="minus" onClick={() => this.insertInput("-")}> - </button>
+				<button className="four" onClick={() => this.insertInput(4)}> 4 </button>
+				<button className="five" onClick={() => this.insertInput(5)}> 5 </button>
+				<button className="six" onClick={() => this.insertInput(6)}> 6 </button>
+				<button className="plus" onClick={() => this.insertInput("+")}> + </button>
+				<button className="one" onClick={() => this.insertInput(1)}> 1 </button>
+				<button className="two" onClick={() => this.insertInput(2)}> 2 </button>
+				<button className="three" onClick={() => this.insertInput(3)}> 3 </button>
+				<button className="equal" onClick={() => this.insertInput("=")}> = </button>
+				<button className="zero" onClick={() => this.insertInput(0)}> 0 </button>
+				<button className="dot" onClick={() => this.insertInput(".")}> . </button>
 			</div>
 		</div>
 		);
